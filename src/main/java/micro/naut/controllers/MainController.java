26a4075310baf1ff2869984ceba9a59d930d1510
Karanbir Singh <krnbr@live.in>
2019-01-14 16:51:10 +0530

    change for the main controller address

--- src/main/java/micro/naut/controllers/MainController.java
+++ src/main/java/micro/naut/controllers/MainController.java
@@ -3,13 +3,15 @@ package micro.naut.controllers;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
 
+import static micro.naut.controllers.Routes.*;
+
 /**
  * Created by Karanbir Singh on 1/13/2019.
  **/
-@Controller(value = "/")
+@Controller(value = BASE_PATH_URI)
 public class MainController {
 
-    @Get(value = Routes.VERSION+"/${micronaut.application.name}/"+Routes.HOME+"/")
+    @Get(value = HOME_URI)
     public String home(){
         return "test";
     }
--- src/main/java/micro/naut/controllers/Routes.java
+++ src/main/java/micro/naut/controllers/Routes.java
@@ -9,10 +9,20 @@ public class Routes {
 
     public static final String VERSION = "v1";
 
+    public static final String SLASH = "/";
+
+    public static final String VERSION_URI = VERSION+SLASH;
+
     @Value("${micronaut.application.name}")
     public String SERVICE_NAME;
 
     public static final String HOME = "home";
+
+    public static final String HOME_URI = HOME+SLASH;
+
+    public static final String BASE_PATH = VERSION_URI+"${micronaut.application.name}";
+
+    public static final String BASE_PATH_URI = SLASH+BASE_PATH+SLASH;
 
     public String HOME_URL = VERSION.concat("/").concat(SERVICE_NAME).concat("/").concat(HOME);
 
